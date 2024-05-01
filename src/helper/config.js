95308f255014a5433b5a270e05acf79b80db90a3
Tajul Islam Refath <tajul16-447@diu.edu.bd>
2024-05-01 12:20:00 +0600

    update: Update base url to production url

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,1 +1,2 @@
-export const BaseURL="http://localhost:5050/api/v1";
+// export const BaseURL="http://localhost:5050/api/v1";
+export const BaseURL = "https://inventory-mern-backend-theta.vercel.app/api/v1";
